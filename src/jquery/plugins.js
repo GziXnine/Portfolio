31d0d9a678b626c40d35039fc0899be7f0e10a28
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-11-28 21:43:03 +0200

    Edit A Media Queuies For All Website

--- src/jquery/plugins.js
+++ src/jquery/plugins.js
@@ -1,7 +1,7 @@
 /** @format */
 
 $(() => {
-  // // !Start A WOW Plugins
+  // !Start A WOW Plugins
   // new WOW().init();
 
   // !Start A VanillaTilt Plugins
@@ -10,11 +10,6 @@ $(() => {
     speed: 400,
     scale: 1.1,
   });
-
-  // // !Global error handler
-  // $(window).on("error", () => {
-  //   window.location.href = "error.html";
-  // });
 
   // ! An Animation For Porfolio
   function toggleParticles() {
@@ -89,24 +84,6 @@ $(() => {
     toggleParticles();
   });
 
-  // function toggleContainer() {
-  //   if ($(window).width() < 1200) {
-  //     $(".container").removeClass("container").addClass("container-fluid");
-  //   } else {
-  //     $(".container-fluid")
-  //       .removeClass("container-fluid")
-  //       .addClass("container");
-  //   }
-  // }
-
-  // // Run on load
-  // toggleContainer();
-
-  // // Run on window resize
-  // $(window).on("resize", function () {
-  //   toggleContainer();
-  // });
-
   var portfolioIsotope;
 
   // Initialize Isotope once the images are loaded
@@ -170,6 +147,41 @@ $(() => {
       .find("a")
       .removeClass("active");
   });
+
+  const $svg = $("#loader");
+  const startShape = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
+  const endShape = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
+
+  // Timeline for animations
+  const tl = gsap.timeline();
+
+  // Animation for text fading out
+  tl.to(".loader-container .loaded", {
+    delay: 1.2,
+    y: -50,
+    opacity: 0,
+    duration: 0.6,
+  });
+
+  // Animate the SVG morphing from start shape to end shape
+  tl.to($svg[0], {
+    duration: 0.8,
+    attr: { d: startShape },
+    ease: "power1.easeIn",
+  }).to($svg[0], {
+    duration: 0.8,
+    attr: { d: endShape },
+    ease: "power1.easeOut",
+  });
+
+  // Move and hide the preloader
+  tl.to(".preloader", {
+    y: -1000,
+    duration: 1,
+  }).to(".preloader", {
+    zIndex: -1,
+    display: "none",
+  });
 });
 
 $(".animateText").textition({
@@ -198,63 +210,3 @@ const timer = setInterval(() => {
 
   $year.text(myComingBday - 1);
 }, 1000);
-
-(function ($) {
-  "use strict";
-
-  $(window).on("load", function () {
-    const $svg = $("#loader");
-    const startShape = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
-    const endShape = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
-
-    // Timeline for animations
-    const tl = gsap.timeline();
-
-    // Animation for text fading out
-    tl.to(".loader-container .loaded", {
-      delay: 1.2,
-      y: -50,
-      opacity: 0,
-      duration: 0.6,
-    });
-
-    // Animate the SVG morphing from start shape to end shape
-    tl.to($svg[0], {
-      duration: 0.8,
-      attr: { d: startShape },
-      ease: "power1.easeIn",
-    }).to($svg[0], {
-      duration: 0.8,
-      attr: { d: endShape },
-      ease: "power1.easeOut",
-    });
-
-    // Move and hide the preloader
-    tl.to(".preloader", {
-      y: -1000,
-      duration: 1,
-    }).to(".preloader", {
-      zIndex: -1,
-      display: "none",
-    });
-  });
-})(jQuery);
-
-/*----------------------------------------------
-3. Smooth Scroll
-----------------------------------------------*/
-// (function ($) {
-//   "use strict";
-
-//   const lenis = new Lenis();
-
-//   lenis.on("scroll", function () {
-//     ScrollTrigger.update();
-//   });
-
-//   gsap.ticker.add(function (time) {
-//     lenis.raf(time * 1000);
-//   });
-
-//   gsap.ticker.lagSmoothing(0);
-// })(jQuery);
